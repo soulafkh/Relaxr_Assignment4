ab2cb0b25ca1165f21d4619e03477adf36504f24
soulafkh <soulaf.khalifeh@gmail.com>
2017-10-23 14:42:54 +0100

    initial commit

---
+++ js/main.js
@@ -0,0 +1,23 @@
+$(document).ready(function() {
+
+$('a').on('click', function(e) {
+  e.preventDefault();
+});
+
+$('.readmore').on('click', function() {
+	$(this).hide();
+	$(this).siblings().slideDown(80);
+});
+
+$('.readless').on('click', function() {
+	$(this).hide();
+	$(this).prev().slideUp(80);
+	$(this).siblings('.readmore').show();
+});
+
+$('.learnmore').on('click', function() {
+	$(this).hide();
+	$(this).next().slideDown();
+});
+
+});
